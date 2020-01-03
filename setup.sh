7605a97bf2df66b69b7f3cdc086f441cdb80955c
Tristan BONNESON <trbonnes@e2r4p14.42.fr>
2020-01-03 12:04:13 +0100

    wordpress ok

--- setup.sh
+++ setup.sh
@@ -1,14 +1,11 @@
 minikube start --vm-driver=virtualbox
 minikube addons enable ingress
-kubectl create secret generic mysql-secrets --from-literal=rootpw=secretpw
-kubectl get secrets
-kubectl create -f srcs/mysql_volume.yaml
-kubectl create -f srcs/mysql.yaml
-kubectl get po
-kubectl get pv
-kubectl create -f srcs/wordpress.yaml
-kubectl get po
-kubectl create -f srcs/nginx.yaml
+kubectl apply -k srcs
+kubectl apply -f srcs/wordpress.yaml
+kubectl apply -f srcs/nginx.yaml
+kubectl apply -f srcs/nginx_deployment_ingress.yaml
 kubectl get services
+minikube ip
+minikube service wordpress --url
 minikube dashboard
 curl -L -i 35.205.31.129
