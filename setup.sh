6a55309ee8aee5b3f2ad209944e45480bcda48c6
Tristan BONNESON <trbonnes@e2r4p14.42.fr>
2020-01-03 17:46:01 +0100

    pma

--- setup.sh
+++ setup.sh
@@ -1,11 +1,10 @@
-minikube start --vm-driver=virtualbox
+minikube start --vm-driver=virtualbox --cpus 5 --memory=5000mb
 minikube addons enable ingress
 kubectl apply -k srcs
-kubectl apply -f srcs/wordpress.yaml
 kubectl apply -f srcs/nginx.yaml
 kubectl apply -f srcs/nginx_deployment_ingress.yaml
 kubectl get services
 minikube ip
-minikube service wordpress --url
+minikube service wordpress-service --url
 minikube dashboard
 curl -L -i 35.205.31.129
