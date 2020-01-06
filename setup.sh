d0bfc54ed6cc4d68a83c384c729e99111f7d9e6c
Tristan BONNESON <trbonnes@e2r3p14.42.fr>
2020-01-06 15:09:50 +0100

    grafana?

--- setup.sh
+++ setup.sh
@@ -1,8 +1,13 @@
 minikube start --vm-driver=virtualbox --cpus 5 --memory=5000mb
 minikube addons enable ingress
-kubectl apply -k srcs
-kubectl apply -f srcs/nginx.yaml
-kubectl apply -f srcs/nginx_deployment_ingress.yaml
+kubectl apply -k srcs/mysql
+kubectl apply -k srcs/influxdb
+kubectl create configmap grafana-config \
+  --from-file=influxdb-datasource.yml=srcs/influxdb/influxdb-datasource.yml \
+  --from-file=grafana-dashboard-provider.yml=srcs/influxdb/grafana-dashboard-provider.yml \
+  --from-file=grafana_dashboard.json=srcs/influxdb/grafana_dashboard.json
+kubectl apply -f srcs/nginx/nginx.yaml
+kubectl apply -f srcs/nginx/nginx_deployment_ingress.yaml
 kubectl get services
 minikube ip
 minikube service wordpress-service --url
