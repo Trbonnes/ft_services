2d54789018682d42d27020feb4b24e5a559d58dd
Tristan BONNESON <trbonnes@e2r3p13.42.fr>
2020-01-07 16:00:26 +0100

    ntm grafana and heapster

--- setup.sh
+++ setup.sh
@@ -1,11 +1,13 @@
 minikube start --vm-driver=virtualbox --cpus 5 --memory=5000mb
 minikube addons enable ingress
+minikube addons enable heapster
 kubectl apply -k srcs/mysql
-kubectl apply -k srcs/influxdb
-kubectl create configmap grafana-config \
-  --from-file=influxdb-datasource.yml=srcs/influxdb/influxdb-datasource.yml \
-  --from-file=grafana-dashboard-provider.yml=srcs/influxdb/grafana-dashboard-provider.yml \
-  --from-file=grafana_dashboard.json=srcs/influxdb/grafana_dashboard.json
+#kubectl apply -k srcs/influxdb
+kubectl apply -k srcs/grafana
+#kubectl create configmap grafana-config \
+#  --from-file=influxdb-datasource.yml=srcs/influxdb/influxdb-datasource.yml \
+#  --from-file=grafana-dashboard-provider.yml=srcs/influxdb/grafana-dashboard-provider.yml \
+#  --from-file=grafana_dashboard.json=srcs/influxdb/grafana_dashboard.json
 kubectl apply -f srcs/nginx/nginx.yaml
 kubectl apply -f srcs/nginx/nginx_deployment_ingress.yaml
 kubectl get services
